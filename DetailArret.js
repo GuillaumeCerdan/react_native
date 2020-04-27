008a2065cad864694cd163afde1fef773631b9fe
Guillaume <gcerdan83@gmail.com>
2020-04-27 13:06:45 +0200

    init

---
+++ DetailArret.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+
+export default class DetailArretScreen extends React.Component {
+
+    componentDidMount() {
+        console.log('on est passé');
+    }
+
+    render() {
+        return (
+            <Text>Coucou</Text>
+        );
+    }
+
+} 
