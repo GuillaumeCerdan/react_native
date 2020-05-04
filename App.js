dc54c007fc0b1c1a1fe9f6729bcc23f8ccd41e24
Guillaume <gcerdan83@gmail.com>
2020-05-04 14:21:40 +0200

    Fix all in HomeScreen

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Storage from 'react-native-storage';
-import { StyleSheet, Text, View, AsyncStorage, FlatList, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
+import { StyleSheet, Text, View, AsyncStorage } from 'react-native';
 
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
@@ -25,26 +25,11 @@ export default class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      allArretes: undefined
-    }
   }
 
   render() {
     return(
-      <View style={styles.view}>
-        <FlatList
-          data={this.state.allArretes}
-          renderItem={({item}) => <Item title={item.name} date={item.date} arrete={item.arrete} style={styles.item}/>}
-          keyExtractor={({id}, index) => id}
-        />
-        <TouchableOpacity
-          title="Press me"
-          color="#66CDAA"
-          onPress={this.alertMe}
-          activeOpacity={0.9}
-          style={styles.button}><Text style={styles.textButton}>TEST BOUTON</Text></TouchableOpacity>
-      </View>
+      <AppContainer />
     );
     // return <AppContainer />;
   }
@@ -55,35 +40,11 @@ export default class App extends React.Component {
       // const {navigate} = this.props.navigation;
       // navigate("Splash");
     }
-
-    fetch("http://www.getup.agency/dev/react/api.php")
-      .then((response) => response.json())
-      .then(data => {
-        this.setState({
-          allArretes: data
-        }, function(){
-    
-        });
-      })
-      .catch(function (error) {
-        alert('Il y a eu un problème avec l\'opération fetch: ' + error.message);
-      });      
       
   }
 
 }
 
-function Item({ title, date, arrete }) {
-  return (
-    <TouchableWithoutFeedback onPress={() => alert(title)}>
-      <View style={styles.item}>
-        <Text style={styles.title}>{title}</Text>
-        <Text style={styles.smallText}>{date}</Text>
-        <Text style={styles.arrete}>{arrete}{arrete}{arrete}</Text>
-      </View>
-    </TouchableWithoutFeedback>
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -91,49 +52,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  bigBlue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
-    textTransform: "uppercase"
-  },
-  red: {
-    color: 'red',
-  },
-  item: {
-    flex: 1,
-    padding: 20
-  },
-  view: {
-    flex: 1, 
-    paddingTop:28,
-    backgroundColor: "#F3F3F3"
-  },
-  title: {
-    fontWeight: 'bold',
-    fontSize: 20, 
-    color: "#000"
-  },
-  prefecture: {
-    padding: 4,
-    fontWeight: "bold",
-    backgroundColor: "#1FCDA3",
-    alignSelf: "flex-start",
-    marginTop: 10
-  },
-  button: {
-    backgroundColor: "#1FCDA3",
-    padding: 20
-  },
-  textButton: {
-    color: "#ffffff",
-    textAlign: "center"
-  },
-  arrete: {
-    paddingTop: 5
-  },
-  smallText: {
-    
   }
 });
--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,16 +1,111 @@
 import React, { Component } from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, FlatList, TouchableOpacity, TouchableWithoutFeedback, StyleSheet } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 export default class HomeScreen extends Component {
-    render() {
-      return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>Home Screen</Text>
-            <Button
-            title="Go to Splash"
-            onPress={() => this.props.navigation.navigate('Splash')}/>
-        </View>
-      )
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      allArretes: undefined
     }
   }
+
+  render() {
+    return (
+      <View style={styles.view}>
+        <FlatList
+          data={this.state.allArretes}
+          renderItem={({ item }) => <Item title={item.name} date={item.date} arrete={item.arrete} style={styles.item} />}
+          keyExtractor={({ id }, index) => id.toString()}
+        />
+        <TouchableOpacity
+          title="Press me"
+          color="#66CDAA"
+          onPress={this.alertMe}
+          activeOpacity={0.9}
+          style={styles.button}><Text style={styles.textButton}>TEST BOUTON</Text></TouchableOpacity>
+      </View>
+    )
+  }
+
+  componentDidMount() {
+    fetch("http://www.getup.agency/dev/react/api.php")
+      .then((response) => response.json())
+      .then(data => {
+        this.setState({
+          allArretes: data
+        }, function () {
+
+        });
+      })
+      .catch(function (error) {
+        alert('Il y a eu un problème avec l\'opération fetch: ' + error.message);
+      });
+  }
+}
+
+function Item({ title, date, arrete }) {
+  return (
+    <TouchableWithoutFeedback onPress={() => alert(title)}>
+      <View style={styles.item}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.smallText}>{date}</Text>
+        <Text style={styles.arrete}>{arrete}{arrete}{arrete}</Text>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+    textTransform: "uppercase"
+  },
+  red: {
+    color: 'red',
+  },
+  item: {
+    flex: 1,
+    padding: 20
+  },
+  view: {
+    flex: 1, 
+    paddingTop:28,
+    backgroundColor: "#F3F3F3"
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 20, 
+    color: "#000"
+  },
+  prefecture: {
+    padding: 4,
+    fontWeight: "bold",
+    backgroundColor: "#1FCDA3",
+    alignSelf: "flex-start",
+    marginTop: 10
+  },
+  button: {
+    backgroundColor: "#1FCDA3",
+    padding: 20
+  },
+  textButton: {
+    color: "#ffffff",
+    textAlign: "center"
+  },
+  arrete: {
+    paddingTop: 5
+  },
+  smallText: {
+    
+  }
+});
