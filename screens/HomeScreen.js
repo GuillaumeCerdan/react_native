bd32e50fe12f1e6d10f94de3508d457f7bb3a549
Getup Agency <acerdan01@gmail.com>
2020-06-15 14:34:55 +0200

    poil de sgeg

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, View, Text, FlatList, TouchableOpacity, TouchableWithoutFeedback, StyleSheet } from 'react-native';
+import { Button, View, Text, FlatList,Modal,TouchableHighlight, TouchableOpacity, TouchableWithoutFeedback, StyleSheet } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import { useNavigation } from '@react-navigation/native';
@@ -15,15 +15,44 @@ export default class HomeScreen extends Component {
     super(props);
 
     this.state = {
-      allArretes: undefined
+      allArretes: undefined,
+      modalVisible: false
     }
 
   }
 
+  setModalVisible = (visible) => {
+    this.setState({ modalVisible: visible });
+  }
   render() {
+    const { modalVisible } = this.state;
 
     return (
       <View style={styles.view}>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert("Modal has been closed.");
+          }}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>Hello World!</Text>
+
+              <TouchableHighlight
+                style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+                onPress={() => {
+                  this.setModalVisible(!modalVisible);
+                }}
+              >
+                <Text style={styles.textStyle}>Hide Modal</Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </Modal>
+
         <FlatList
           data={this.state.allArretes}
           renderItem={({ item }) => <Item title={item.name} date={this.renderDate(item.date)} prefecture={item.prefecture.name} pinned={item.pinned} style={styles.item} />}
@@ -58,9 +87,9 @@ function OpenSplash({ screenName }) {
   navigation.navigate(screenName)
  
 }
-function Item({ title, date, prefecture, pinned }) {
+function Item({ title, date, prefecture, pinned, modalVisible }) {
   return (
-    <TouchableWithoutFeedback onPress={() => this.OpenSplash('Splash')}>
+    <TouchableWithoutFeedback onPress={() => this.setModalVisible(!modalVisible)}>
       <View style={styles.item}>
 
         <View style={styles.layoutHorizontal}>
@@ -126,5 +155,35 @@ const styles = StyleSheet.create({
   },
   date: {
     fontSize: 14
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+  },
+  openButton: {
+    backgroundColor: "#F194FF",
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
   }
 });
